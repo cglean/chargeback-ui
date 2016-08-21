f7f0b6842600368c78bb8fc5d999cd5f0f38c700
Amit Bansal <amitbansal26@gmail.com>
2016-08-21 23:41:35 +0530

    Made URL Changes

--- src/main/java/com/chargeback/rest/clients/ChargeBackApiRestClient.java
+++ src/main/java/com/chargeback/rest/clients/ChargeBackApiRestClient.java
@@ -9,28 +9,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.chargeback.util.RestClientConstants;
 import com.chargeback.vo.UsageRecord;
 
-/**
- * 
- * @author amit
- *
- */
-@FeignClient(RestClientConstants.METRICS_SERVICE)
+
+@FeignClient("metrics-service")
 public interface ChargeBackApiRestClient {
 
 	
-	@RequestMapping(value=RestClientConstants.INSTANCE_METRICS, method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value="/metrics/getInstanceMetrics", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<UsageRecord>> fetchInstanceMetrics();
 	
-	@RequestMapping(value=RestClientConstants.ORG_LIST, method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value="/metrics/getOrgList", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<String>> getOrgList();
 		
-	@RequestMapping(value=RestClientConstants.FREE_RESOURCE, method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> getFreeResourceValue(@PathVariable String resourceType);
+	@RequestMapping(value="/metrics/getFreeResource/{resourceType}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> getFreeResourceValue(@PathVariable(value="resourceType") String resourceType);
 	
-	@RequestMapping(value=RestClientConstants.SPACE_LIST, method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<String>> getSpaceList(@PathVariable String orgName);
+	@RequestMapping(value="/metrics/getSpaceList{orgName}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<String>> getSpaceList(@PathVariable(value="orgName") String orgName);
 	
 }
--- src/main/java/com/chargeback/util/RestClientConstants.java
+++ src/main/java/com/chargeback/util/RestClientConstants.java
@@ -9,7 +9,7 @@ public class RestClientConstants {
 	public static final String INSTANCE_METRICS = "/metrics/getInstanceMetrics";
 	public static final String ORG_LIST = "/metrics/getOrgList";
 	public static final String FREE_RESOURCE = "/metrics/getFreeResource/{resourceType}";
-	public static final String SPACE_LIST = "/metrics/getSpaceList{orgName}";
+	public static final String SPACE_LIST = "/metrics/getSpaceList/{orgName:.+}";
 	public static final String GET_RESOURCE_DETAILS = "/getResourceDetails/{usageType}/{resourceType}/{orgName:.+}/{space:.+}";
 	public static final String GET_ORG_LIST = "/getOrgList";
 	public static final String GET_SPACE_LIST ="/getSpaceList/{orgName:.+}";
