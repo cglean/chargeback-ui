5c61053e1ae74ddbd7d0a5c24b5183a610515a7f
Amit Bansal <amitbansal26@gmail.com>
2016-08-21 14:25:34 +0530

    Refactored

--- src/main/java/com/chargeback/ChargebackMicroApplication.java
+++ src/main/java/com/chargeback/ChargebackMicroApplication.java
@@ -3,18 +3,26 @@ package com.chargeback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EnableEurekaClient
+@EnableFeignClients
 public class ChargebackMicroApplication {
 
 	
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 	    return new RestTemplate();
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ChargebackMicroApplication.class, args);
 	}
+	
+	
 }
--- src/main/java/com/chargeback/controller/ChargeBackController.java
+++ src/main/java/com/chargeback/controller/ChargeBackController.java
@@ -5,37 +5,29 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
+import com.chargeback.rest.clients.ChargeBackApiRestClient;
 import com.chargeback.vo.ChartVO;
 import com.chargeback.vo.UsageRecord;
 
-
 /**
- *  This Controller gives the Chart Data after calling the metrics service to the UI.
+ * This Controller gives the Chart Data after calling the metrics service to the
+ * UI.
+ * 
  * @author ambansal
  *
  */
 @RestController
 public class ChargeBackController {
 
-	// TODO :: Need to fetch this from Eureka Server Client Id by just giving application name 
-	private static final String INSTANCE_METRICS_URL = "http://chargeback-api.cfapps.io/metrics/getInstanceMetrics";
-	private static final String FREERESOURRCE_URL = "http://chargeback-api.cfapps.io/metrics/getFreeResource";
-	
-	private static final String ORG_LIST_URL = "http://chargeback-api.cfapps.io/metrics/getOrgList";
-	private static final String SPACELIST_URL = "http://chargeback-api.cfapps.io/metrics/getSpaceList";
-
-	@Autowired  private RestTemplate restTemplate; 
+	@Autowired
+	ChargeBackApiRestClient metricsClient;
 
 	/**
 	 * 
@@ -45,70 +37,70 @@ public class ChargeBackController {
 	 * @param space
 	 * @return
 	 */
-	@RequestMapping(value="/getResourceDetails/{usageType}/{resourceType}/{orgName:.+}/{space:.+}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	 public ChartVO getResourceUsage(@PathVariable String usageType , @PathVariable String resourceType,  @PathVariable String orgName, @PathVariable String space){
-		final ResponseEntity<List<UsageRecord>> response = restTemplate.exchange(INSTANCE_METRICS_URL, HttpMethod.GET, HttpEntity.EMPTY,
-				new ParameterizedTypeReference<List<UsageRecord>>() {
-				});
+	@RequestMapping(value = "/getResourceDetails/{usageType}/{resourceType}/{orgName:.+}/{space:.+}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ChartVO getResourceUsage(@PathVariable String usageType, @PathVariable String resourceType,
+			@PathVariable String orgName, @PathVariable String space) {
+
+		final ResponseEntity<List<UsageRecord>> response = metricsClient.fetchInstanceMetrics();
 		Function<ResponseEntity<List<UsageRecord>>, List<String>> usedResourceFunction = null;
 		Function<ResponseEntity<List<UsageRecord>>, List<String>> appLabelFunction = null;
-		
-		if(resourceType.equals("MEM")){
-		usedResourceFunction = usedMemory ->response.getBody()
-				.stream().filter(usageRecord -> (usageRecord.getOrgName().equals(orgName) && usageRecord.getSpaceName().equals(space) ))
-				.map(usageRecord -> usageRecord.getMemory()).collect(Collectors.toList());
-		
-		}else if(resourceType.equals("CPU")){
-			usedResourceFunction = usedCPU ->response.getBody()
-					.stream().filter(usageRecord -> (usageRecord.getOrgName().equals(orgName) && usageRecord.getSpaceName().equals(space) ))
+
+		if (resourceType.equals("MEM")) {
+			usedResourceFunction = usedMemory -> response.getBody().stream()
+					.filter(usageRecord -> (usageRecord.getOrgName().equals(orgName)
+							&& usageRecord.getSpaceName().equals(space)))
+					.map(usageRecord -> usageRecord.getMemory()).collect(Collectors.toList());
+
+		} else if (resourceType.equals("CPU")) {
+			usedResourceFunction = usedCPU -> response.getBody().stream()
+					.filter(usageRecord -> (usageRecord.getOrgName().equals(orgName)
+							&& usageRecord.getSpaceName().equals(space)))
 					.map(usageRecord -> usageRecord.getCpu()).collect(Collectors.toList());
 
-		}else if(resourceType.equals("DISK")){
-			usedResourceFunction = usedCPU ->response.getBody()
-					.stream().filter(usageRecord -> (usageRecord.getOrgName().equals(orgName) && usageRecord.getSpaceName().equals(space) ))
+		} else if (resourceType.equals("DISK")) {
+			usedResourceFunction = usedCPU -> response.getBody().stream()
+					.filter(usageRecord -> (usageRecord.getOrgName().equals(orgName)
+							&& usageRecord.getSpaceName().equals(space)))
 					.map(usageRecord -> usageRecord.getDisk()).collect(Collectors.toList());
 
-		}else{
+		} else {
 			throw new RuntimeException("Please Select Resource Type from : CPU, DISK, MEM");
 		}
-		
-		appLabelFunction = appLabel ->response.getBody()
-				.stream().filter(usageRecord -> (usageRecord.getOrgName().equals(orgName) && usageRecord.getSpaceName().equals(space) )).map(usageRecord -> usageRecord.getAppname().concat(" - ")
-						.concat(usageRecord.getInstanceIndex())).collect(Collectors.toList());
 
-		if(usageType.equals("UNUSED")){
-			if(!resourceType.equals("DISK")){
-			final ResponseEntity<String> freeResourceResponse = restTemplate.exchange(FREERESOURRCE_URL + "/" + resourceType, HttpMethod.GET, HttpEntity.EMPTY,
-					new ParameterizedTypeReference<String>() {
-					});
-			return getUnUsedResource(response, freeResourceResponse, usedResourceFunction, appLabelFunction);
-			}else{
+		appLabelFunction = appLabel -> response.getBody().stream().filter(
+				usageRecord -> (usageRecord.getOrgName().equals(orgName) && usageRecord.getSpaceName().equals(space)))
+				.map(usageRecord -> usageRecord.getAppname().concat(" - ").concat(usageRecord.getInstanceIndex()))
+				.collect(Collectors.toList());
+
+		if (usageType.equals("UNUSED")) {
+			if (!resourceType.equals("DISK")) {
+
+				return getUnUsedResource(response, metricsClient.getFreeResourceValue(resourceType),
+						usedResourceFunction, appLabelFunction);
+			} else {
 				throw new RuntimeException("Not able to get total disk usage as of now");
 			}
 
 		}
 		return getParameterizedUsageDetails(response, usedResourceFunction, appLabelFunction);
-	 }
-	
-@RequestMapping(value="/getOrgList", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-public List<String> getOrganizationNames(){
-	
-	final ResponseEntity<List<String>> response = restTemplate.exchange(ORG_LIST_URL, HttpMethod.GET, HttpEntity.EMPTY,
-			new ParameterizedTypeReference<List<String>>() {
-			});
-	return response.getBody();
-}
+	}
 
-@RequestMapping(value = "/getSpaceList/{orgName:.+}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)	
-public List<String> getSpaceList(@PathVariable String orgName){
-	
-	final ResponseEntity<List<String>> response = restTemplate.exchange(SPACELIST_URL + "/" + orgName , HttpMethod.GET, HttpEntity.EMPTY,
-			new ParameterizedTypeReference<List<String>>() {
-			});
-	return response.getBody();
-}
-	
-	private ChartVO getUnUsedResource(final ResponseEntity<List<UsageRecord>> response, final ResponseEntity<String> freeResourceResponse, Function<ResponseEntity<List<UsageRecord>>, List<String>> freeResourceFunction,Function<ResponseEntity<List<UsageRecord>>, List<String>> appLabelFunction) {
+	@RequestMapping(value = "/getOrgList", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<String> getOrganizationNames() {
+
+		return metricsClient.getOrgList().getBody();
+	}
+
+	@RequestMapping(value = "/getSpaceList/{orgName:.+}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<String> getSpaceList(@PathVariable String orgName) {
+
+		return metricsClient.getSpaceList(orgName).getBody();
+	}
+
+	private ChartVO getUnUsedResource(final ResponseEntity<List<UsageRecord>> response,
+			final ResponseEntity<String> freeResourceResponse,
+			Function<ResponseEntity<List<UsageRecord>>, List<String>> freeResourceFunction,
+			Function<ResponseEntity<List<UsageRecord>>, List<String>> appLabelFunction) {
 		final List<String> freeResource = freeResourceFunction.apply(response);
 		final List<String> appLabel = appLabelFunction.apply(response);
 		freeResource.add(freeResourceResponse.getBody());
@@ -118,16 +110,17 @@ public List<String> getSpaceList(@PathVariable String orgName){
 		chartVO.setLabel(appLabel);
 		return chartVO;
 	}
-	
 
-private ChartVO getParameterizedUsageDetails(final ResponseEntity<List<UsageRecord>> response, Function<ResponseEntity<List<UsageRecord>>, List<String>> resourceUsedFunction, Function<ResponseEntity<List<UsageRecord>>, List<String>> appLabelFunction){
-	
-	final List<String> resourceUsed = resourceUsedFunction.apply(response);
-	final List<String> appLabel = appLabelFunction.apply(response);
-	
-	final ChartVO chartVO = new ChartVO();
-	chartVO.setData(resourceUsed);
-	chartVO.setLabel(appLabel);
-	return chartVO;
-}
+	private ChartVO getParameterizedUsageDetails(final ResponseEntity<List<UsageRecord>> response,
+			Function<ResponseEntity<List<UsageRecord>>, List<String>> resourceUsedFunction,
+			Function<ResponseEntity<List<UsageRecord>>, List<String>> appLabelFunction) {
+
+		final List<String> resourceUsed = resourceUsedFunction.apply(response);
+		final List<String> appLabel = appLabelFunction.apply(response);
+
+		final ChartVO chartVO = new ChartVO();
+		chartVO.setData(resourceUsed);
+		chartVO.setLabel(appLabel);
+		return chartVO;
+	}
 }
